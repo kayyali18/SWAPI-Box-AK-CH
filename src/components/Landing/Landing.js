edff406613a3b16873b7a16ba218dc6a6241a17f
Ahmad Kayyali <kayyali18@gmail.com>
2018-10-08 19:10:26 -0600

    Display desired data in landing

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -6,10 +6,21 @@ class Landing extends Component {
     super(props);
     this.state = {  }
   }
+
+  displayRandomText = () => {
+    let random = this.getRandom(0, 6)
+    return <h2 className='title'>{this.props.films[random].opening_crawl}</h2>
+  }
+
+  getRandom = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min 
+  }
+
   render() { 
+    if (!this.props.films) return null
     return ( 
       <header className='l-landing m-landing'>
-        {this.displayRandomText}
+        {this.displayRandomText()}
       </header>
      );
   }
