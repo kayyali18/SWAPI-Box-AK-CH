ef846087175ef1def56764afc15e71cbd6778de4
Ahmad Kayyali <kayyali18@gmail.com>
2018-10-11 11:58:56 -0600

    Fully test the api

--- src/api.test.js
+++ src/api.test.js
@@ -1,34 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import { createSerializer } from 'enzyme-to-json';
+import React from "react"
+import ReactDOM from "react-dom"
+import { configure } from "enzyme"
+import Adapter from "enzyme-adapter-react-16"
+import { createSerializer } from "enzyme-to-json"
 
-import * as API from './api'
+import * as API from "./api"
 
-configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() })
 
-describe('api', () => {
-  let mockResults
+describe("api", () => {
+  let mockResults, mockCategories
   beforeEach(() => {
-    mockResults = [{blah: 'blah'}, {blegh: 'bluh'}]
+    mockResults = [{ blah: "blah" }, { blegh: "bluh" }]
+    mockCategories = {
+      categories: [
+        { category: "planets", page: 1 },
+        { category: "people", page: 1 },
+        { category: "vehicles", page: 1 },
+        { category: "films", page: 1 }
+      ]
+    }
 
-    window.fetch = jest.fn(() => {
-      Promise.resolve({
-        ok: true,
-        json: () => Promise.resolve(mockResults)
-      })
+    window.fetch = jest.fn(() => Promise.resolve({
+      ok: true,
+      json: () => Promise.resolve(mockCategories)
     })
+    )
   })
 
-  it('should call fetch with the proper params', async () => {
+  it("should call fetch with the proper params", async () => {
     const url = `https://swapi.co/api/films/?page=1`
-    //Execution 
-    await API.fetchData('films', 1)
+    //Execution
+    await API.fetchData("films", 1)
 
     // Expectation
-    expect(window.fetch).toHaveBeenCalled()
+    expect(window.fetch).toHaveBeenCalledWith(url)
   })
 
-  
+  it("fetchData should return some data", async () => {
+    const url = `https://swapi.co/api/films/?page=1`
+
+    //Execution
+    const returned = await API.fetchData("films", 1)
+    
+    // Expectation
+    expect(window.fetch).toHaveBeenCalled()
+    expect(returned).toEqual(mockCategories)
+  })
 })
