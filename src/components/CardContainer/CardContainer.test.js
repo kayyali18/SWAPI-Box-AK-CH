d317d163d5508ba42ce65429f377133517bb85a8
Ahmad Kayyali <kayyali18@gmail.com>
2018-10-11 12:08:12 -0600

    Test Card Container

---
+++ src/components/CardContainer/CardContainer.test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+ import ReactDOM from 'react-dom'
+import CardContainer from './CardContainer'
+import {shallow, configure} from 'enzyme'
+
+describe('Card Container', () => {
+  let wrapper
+  let mockProps
+
+
+  beforeEach(() => {
+    wrapper = shallow(<CardContainer />)
+  })
+
+  it('renders without crashing', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+})
