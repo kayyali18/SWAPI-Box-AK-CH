5804a5aecd12fb925b8bb440a3660081d3de5dfd
Casey Hoepner <caseyhoepner@gmail.com>
2018-10-18 22:28:01 -0600

    Add photos to people cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -33,35 +33,27 @@ class Card extends Component {
         className="display-card"
         aria-label="Individual display of results"
       >
-        <div className="card-content">
           <h3>{data.name}</h3>
-          <p>Terrain: {data.terrain}</p>
-          <p>Population: {data.population}</p>
-          <p>Climate: {data.climate}</p>
+            <p>Terrain: {data.terrain}</p>
+            <p>Population: {data.population}</p>
+            <p>Climate: {data.climate}</p>
           {this.whoLivesHere()}
-        </div>
-        <div className="card-image" />
       </article> 
     )
   }
 
   peopleDisplay = () => {
     const { data, category } = this.props
+    const joinedName = (data.main.name).split(' ').join('')
 
     return (
       <article
-        className="display-card"
-        aria-label="Individual display of results"
-      >
-        <div className="card-content">
-          <h3>{data.main.name}</h3>
-          <ul>
-            <li>Homeworld: {data.supp.homeworld}</li>
-            <li>Species: {data.supp.species}</li>
-            <li>Population: {data.supp.population}</li>
-          </ul>
-        </div>
-        <div className="card-image" />
+        className={`display-card ${joinedName}`}
+        aria-label="Individual display of results">
+          <h2>{data.main.name}</h2>
+            <p>Homeworld: {data.supp.homeworld}</p>
+            <p>Species: {data.supp.species}</p>
+            <p>Population: {data.supp.population}</p>
       </article>
     )
   }
@@ -80,13 +72,10 @@ class Card extends Component {
           className="display-card"
           aria-label="Individual display of results"
         >
-          <div className="card-content">
             <h3>{data.name}</h3>
-            <p>Model: {data.model}</p>
-            <p>Class: {data.vehicle_class}</p>
-            <p>Number of Passengers: {data.passengers}</p>
-          </div>
-          <div className="card-image" />
+              <p>Model: {data.model}</p>
+              <p>Class: {data.vehicle_class}</p>
+              <p>Number of Passengers: {data.passengers}</p>
         </article>
       )
     } else return <h1>Loading </h1>
