783bd1e3ebbb10a8d69fe7d7101614a66262a36c
Ahmad Kayyali <kayyali18@gmail.com>
2018-10-22 08:58:14 -0600

    Run eslint

--- src/api.js
+++ src/api.js
@@ -1,81 +1,83 @@
-export const fetchData = async (category) => {
+export const fetchData = async category => {
   const url = `https://swapi.co/api/${category}/`
   let data, suppData, filteredData
   let item = JSON.parse(localStorage.getItem(category))
-  let favs = JSON.parse(localStorage.getItem('favourites'))
-  if (favs == null) localStorage.setItem('favourites', JSON.stringify([]))
+  let favs = JSON.parse(localStorage.getItem("favourites"))
+  if (favs === null) localStorage.setItem("favourites", JSON.stringify([]))
 
   if (item) return item
-  console.log('didnt return')
   try {
     switch (category) {
-      case 'people':
+      case "people":
         data = await fetchByURL(url)
         suppData = await fetchPeopleExtras(data)
         filteredData = await filterData(data.results, suppData)
-        localStorage.setItem('people', JSON.stringify(filteredData))
+        localStorage.setItem("people", JSON.stringify(filteredData))
 
         return filteredData
 
-      case 'planets':
+      case "planets":
         data = await fetchByURL(url)
         suppData = await fetchPlanetExtras(data)
         filteredData = await filterData(data.results, suppData)
-        localStorage.setItem('planets', JSON.stringify(filteredData))
+        localStorage.setItem("planets", JSON.stringify(filteredData))
 
         return filteredData
 
-      case 'films':
+      case "films":
         data = await fetchByURL(url)
-        localStorage.setItem('films', JSON.stringify(data))
+        localStorage.setItem("films", JSON.stringify(data))
 
         return data
 
-      case 'vehicles':
+      case "vehicles":
         data = await fetchByURL(url)
         filteredData = addKey([...data.results], category)
-        localStorage.setItem('vehicles', JSON.stringify(filteredData))
+        localStorage.setItem("vehicles", JSON.stringify(filteredData))
 
         return filteredData
 
       default:
-        console.log('Sorry.')
+        return null
     }
-
   } catch (error) {
-    console.log(error)
+    return error
   }
 }
 
 const addKey = (data, category) => {
   let newData = []
-  data.forEach(entry => newData.push({currCategory: category, key: entry.name, main: entry}))
+  data.forEach(entry =>
+    newData.push({ currCategory: category, key: entry.name, main: entry })
+  )
   return newData
 }
 
 const filterData = (data, suppData) => {
   const filteredData = data.map((person, index) => {
-    const combined = {main: person, supp: suppData[index], key: person.name}
-    console.log (combined)
+    const combined = { main: person, supp: suppData[index], key: person.name }
     return combined
   })
   return filteredData
 }
 
-export const checkStorage = (item) => {
+export const checkStorage = item => {
   const thing = localStorage.getItem(JSON.parse(item))
 
-  if (thing) return thing 
-  
-  console.log(thing)
+  if (thing) return thing
+
   return thing
 }
 
-export const fetchPeopleExtras = async (data) => {
+export const fetchPeopleExtras = async data => {
   const extras = data.results.map(async entry => {
     let homeworld = await fetchByURL(entry.homeworld)
     let species = await fetchByURL(entry.species)
-    let combined = { homeworld: homeworld.name, population: homeworld.population, species: species.name }
+    let combined = {
+      homeworld: homeworld.name,
+      population: homeworld.population,
+      species: species.name
+    }
 
     return combined
   })
@@ -83,34 +85,31 @@ export const fetchPeopleExtras = async (data) => {
   return Promise.all(extras)
 }
 
-export const fetchPlanetExtras = async (data) => {
+export const fetchPlanetExtras = async data => {
   const extras = data.results.map(async entry => {
     let residents = await entry.residents.map(async res => {
       let name = await fetchByURL(res)
       return name.name
     })
-  return Promise.all(residents)
+    return Promise.all(residents)
   })
   return Promise.all(extras)
 }
 
-export const fetchByURL = async (url) => {
+export const fetchByURL = async url => {
   try {
     const response = await fetch(url)
     const data = await response.json()
 
     return data
-
-  } catch (error) {
-    console.log(error)
-  }
+  } catch (error) {}
 }
 
 export const getFavs = () => {
-  let favs = JSON.parse(localStorage.getItem('favourites'))
+  let favs = JSON.parse(localStorage.getItem("favourites"))
   return favs
 }
 
-export const updateFav = (newFavs) => {
-  localStorage.setItem('favourites', JSON.stringify(newFavs))
+export const updateFav = newFavs => {
+  localStorage.setItem("favourites", JSON.stringify(newFavs))
 }
